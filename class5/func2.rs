cfd657d367b6c59d0d4bbeb6a3105e4d1af33d0a
Pro696969 <krishnakumaarmc@gmail.com>
2024-02-16 15:14:02 +0530

    func3

--- class5/func2.rs
+++ class5/func2.rs
@@ -1,7 +1,31 @@
 use std::io;
 fn mul(x:i32, y:i32) -> i32 {
-    x*y
+     x*y
 }
 fn main() {
-    mul(5,3);
+    let mut a = String::new();
+    io::stdin().read_line(&mut a)
+        .expect("fail");
+    let a: i32 = match a.trim().parse() {
+        Ok(a) => a, 
+        Err(_) => {
+            println!("error");
+            return;
+        }
+    };
+
+    let mut b = String::new();
+    io::stdin().read_line(&mut b)
+        .expect("fail");
+    let b: i32 = match b.trim().parse() {
+        Ok(b) => b, 
+        Err(_) => {
+            println!("error");
+            return;
+        }
+    };
+    
+    println!("{}", mul(a,b));
+
 }
+
